1c881c32adc860b172671f0df8fc04f1dfce6137
GuiPines <gspinho888@gmail.com>
2024-06-08 18:45:34 +0100

    Update attack.js

--- API/attack.js
+++ API/attack.js
@@ -268,4 +268,154 @@ router.put("/attackCharacter", (req, res) => {
     };
 });
 
+
+router.put("/attackCPU", (req, res) => {
+    var playerID = req.body.playerID;
+    var attackerID = req.body.attackerID;
+    var matchID = req.body.matchID;
+
+    var CPU_x;
+    var CPU_y = 4;
+
+    if (!playerID || !attackerID || !matchID){
+        res.send("Hey sorry there's data missing I can't let u continue :(");
+        return;
+    }
+
+    connection.execute("SELECT match_player1_id AS player1, match_turn FROM match_ WHERE match_id = ?", 
+        [matchID],
+        function (error, rows, fields){
+            if (error){
+                res.send(error)
+            } else {
+                //console.log(rows);
+                var FirstPlayerID = rows[0].player1;
+                var Turn = rows[0].match_turn;
+                if(Turn % 2 == 0){
+                    CPU_x = 1;
+                    if (FirstPlayerID == playerID){
+                        res.send("It's not your turn yet! Be patient.")
+                        return;
+                    } else {
+                        //console.log(rows);
+                        attackCPU_Starts();
+                    };
+                } else {
+                    CPU_x = 9;
+                    if (FirstPlayerID == playerID){
+                        //console.log(rows);
+                        attackCPU_Starts();
+                    } else {
+                        res.send("It's not your turn yet! Be patient.")
+                        return;
+                    };
+                };
+            };
+        }
+    );
+
+    function attackCPU_Starts(){
+        connection.execute("SELECT mpc_cha_id FROM match_player_character WHERE mpc_match_id = ? AND mpc_mp_id = ? AND mpc_board_slot = ?", //get ID of the type of the attacker character.
+            [matchID, playerID, attackerID],
+            function (error, rows, fields){
+                if (error){
+                    res.send(error)
+                } else {
+                    //console.log(rows);
+                    var attackerTypeID = rows[0].mpc_cha_id;
+                    connection.execute("SELECT cha_range, mpc_tile_x, mpc_tile_y FROM character_, match_player_character WHERE cha_id = "+ attackerTypeID +" AND mpc_match_id = ? AND mpc_mp_id = ? AND mpc_board_slot = ?", //get the range and position of the character that's attacking.
+                        [matchID, playerID, attackerID],
+                        function (error, rows, fields){
+                            if (error){
+                                res.send(error)
+                            } else {
+                                //console.log(rows);
+                                var Attacker_range = rows[0].cha_range;
+                                var Attacker_x = rows[0].mpc_tile_x;
+                                var Attacker_y = rows[0].mpc_tile_y;
+
+                                // To calculate the distance between the attacker and the target to confirm if it's within range
+                                if (Attacker_range < Math.abs(Attacker_x - CPU_x)){
+                                    res.send("NUH-UH you can't attack the CPU it's OUT OF RANGE!");
+                                    return;
+                                } else {
+                                    if (Attacker_range < Math.abs(Attacker_y - CPU_y)){
+                                        res.send("NUH-UH you can't attack the CPU it's OUT OF RANGE!");
+                                        return;
+                                    } else {
+
+                                        connection.execute("SELECT cha_atk FROM character_ WHERE cha_id = "+ attackerTypeID +"", //get the DMG value of the character thats gonna attack
+                                            function (error, rows, fields) {
+                                                if (error){
+                                                    res.send(error);
+                                                } else {
+                                                    //console.log(rows);
+                                                    var Damage = rows[0].cha_atk; //store the value from select in a variable
+                                                    //console.log("Damage = " + Damage);
+                                                    connection.execute("UPDATE match_player SET mp_current_cpu_hp = mp_current_cpu_hp - " + Damage + " WHERE mp_match_id = ? AND NOT mp_player_id = ?", //damage CPU, making sure to target the right match and the player that is not the attacker
+                                                        [matchID, playerID],
+                                                        function (error, rows, fields) {
+                                                            if (error){
+                                                                res.send(error);
+                                                                //console.log(rows);
+                                                            } else {
+                                                                connection.execute("SELECT mp_current_cpu_hp FROM match_player WHERE mp_match_id = ? AND NOT mp_player_id = ?",
+                                                                    [matchID, playerID],
+                                                                    function (error, rows, fields){
+                                                                        if (error){
+                                                                            res.send(error);
+                                                                        } else {
+                                                                            //console.log(rows);
+                                                                            var CPU_Health = rows[0].mp_current_cpu_hp;
+                                                                            connection.execute("SELECT match_turn FROM match_ WHERE match_id = ?", //get match turn value
+                                                                                [matchID],
+                                                                                function (error, rows, fields){
+                                                                                    if (error){
+                                                                                        res.send(error);
+                                                                                    } else {
+                                                                                        //console.log(rows);
+                                                                                        var Turn = rows[0].match_turn; //store value in a variable
+                                                                                        //console.log("Turn = " + Turn);
+                                                                                        connection.execute("UPDATE match_ SET match_turn = match_turn + 1 WHERE match_id = ?", //increase match turn by 1
+                                                                                            [matchID],
+                                                                                            function (error, rows, fields){
+                                                                                                if (error){
+                                                                                                    res.send(error);
+                                                                                                    //console.log(rows);
+                                                                                                } else {
+                                                                                                    console.log("Trocou a turn, atacou e...");
+                                                                                                    if (CPU_Health <= 0){
+                                                                                                        console.log("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+                                                                                                        console.log(matchID, playerID, targetID) 
+                                                                                                        res.send("PUTO GANHASTE WHAAAAAAAAAT")
+                                                                                                    } else {
+                                                                                                        console.log("Ainda n mataste o CPU (ainda hehehe)")
+                                                                                                    }
+                                                                                                };
+                                                                                            }
+                                                                                        );
+                                                                                    };
+                                                                                }
+                                                                            );
+                                                                        }
+                                                                    }
+                                                                )
+                                                            };
+                                                        }
+                                                    );
+                                                };
+                                            }
+                                        );
+                                    };
+                                };
+                            };
+                        }
+                    );
+                };
+            }
+        );
+    };
+});
+
+
 module.exports = router;
